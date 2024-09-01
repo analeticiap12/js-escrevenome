202a8b8b2dfb0fc95a02c9d1b1f2d51842c4104c
ana leticia <analeticiap435@gmail.com>
2024-09-01 10:07:14 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,14 @@
+function setup() {
+  createCanvas(600, 600);
+}
 
+function draw() {
+  stroke ("blue");
+  fill ("red")
+  
+  
+  if (mouseIsPressed0){
+    rect (mouseX, mouseY, 20, 35);
+  }
+  
+}
